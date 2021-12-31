67322983d1024ec453aa726ca3d7ff6f4d73ecd7
AlyssaWang <alyssa.c.wang@gmail.com>
2021-12-31 01:43:27 -0500

    remove v1 files

--- README.md
+++
@@ -1,1 +0,0 @@
-# AlyssaWang.github.io
--- behavior/mail-test.php
+++
@@ -1,6 +0,0 @@
-// Not currently supported by Github Pages
-
-<?php
-mail('acwdetheris14@gmail.com','Test mail','The mail function is working!');
-echo 'Mail sent!';
-?>
